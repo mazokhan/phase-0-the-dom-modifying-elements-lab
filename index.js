980af4cfbd07cd3f9baf9a9bb7e4934acbd9b763
mazen <mak9986@hotmail.com>
2021-11-04 13:42:10 +1100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let remobj = document.getElementById("main");
+remobj.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "Thats Great"
